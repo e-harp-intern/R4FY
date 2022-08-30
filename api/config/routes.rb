0f9190ecd8ef257c1da872f3207e4120055e5186
MotomaeTougo <tougo0515@ezweb.ne.jp>
2022-08-30 13:16:13 +0900

    #24 ツアー一覧降順表示

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
         namespace "v1" do
           post "login" => "sessions#create"
           delete "logout" => "sessions#destroy"
+          get "tours" => "toursort#index"
         end
     end
 end
