f8a1889b5c941ef756b8bba17c11079eef483265
MotomaeTougo <tougo0515@ezweb.ne.jp>
2022-08-31 15:05:06 +0900

    #15 コントローラーにコメント追加

--- api/app/controllers/api/v1/admins_controller.rb
+++ api/app/controllers/api/v1/admins_controller.rb
@@ -1,11 +1,17 @@
+# 管理者アカウントを作成するためのコントローラー
 class Api::V1::AdminsController < ApplicationController
+  # 　管理者アカウント作成のメソッド
   def create
+    # 管理者アカウントに必要な情報を取得
     begin
       user = Admin.new(email: params[:email], name: params[:name], password: create_temp_pass).save!
+
+    # バリデーション外であればエラー表示
     rescue StandardError
       render json: json_render_v1(false)
       return
     end
+    # アカウントが作成されたら成功表示
     render json: json_render_v1(true)
   end
 end
--- api/app/controllers/api/v1/guides_controller.rb
+++ api/app/controllers/api/v1/guides_controller.rb
@@ -1,11 +1,17 @@
+# ガイドアカウントを作成するためのコントローラー
 class Api::V1::GuidesController < ApplicationController
+  # ガイドアカウントを作成するメソッド
   def create
+    # ガイドアカウントに必要な情報を取得
     begin
       user = Guide.new(email: params[:email], name: params[:name], memo: params[:memo]).save!
+
+    # バリデーション外であればエラー表示
     rescue StandardError
       render json: json_render_v1(false)
       return
     end
+    # アカウントが作成されたら成功表示
     render json: json_render_v1(true)
   end
 end
