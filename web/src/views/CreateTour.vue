57af3b2cea21438b338b7338ea0211ce2879091b
24momo-Git <24momota1004@gmail.com>
2022-08-31 16:23:59 +0900

    #17 ツアー新規追加画面　作成

---
+++ web/src/views/CreateTour.vue
@@ -0,0 +1,179 @@
+<template>
+  <div id="create-account-page">
+    <h1>{{ $t("pages.tours.create.title") }}</h1>
+    <div class="form-frame">
+      <form @submit.prevent="create" class="form-main">
+        <div class="form-tabel">
+          <!--ツアー名-->
+          <label>{{ $t("label.tour_name") }}</label
+          ><input
+            type="text"
+            :placeholder="$t('label.detail.tour_name')"
+            id="tour_name"
+            required
+          />
+          <!--開始日時-->
+          <label>{{ $t("label.start_datetime") }}</label
+          ><input
+            type="datetime-local"
+            :placeholder="start_datetime"
+            id="start_datetime"
+            required
+          />
+          <!--終了日時-->
+          <label>{{ $t("label.end_datetime") }}</label
+          ><input
+            type="datetime-local"
+            :placeholder="end_datetime"
+            id="end_datetime"
+            required
+          />
+          <!--大人-->
+          <label>{{ $t("label.adult_num") }}</label
+          ><input
+            type="number"
+            :placeholder="$t('label.detail.adult_num')"
+            id="adult_num"
+            required
+          />
+          <!--子供-->
+          <label>{{ $t("label.child_num") }}</label
+          ><input
+            type="number"
+            :placeholder="$t('label.detail.child_num')"
+            id="child_num"
+            required
+          />
+          <!--ガイド-->
+          <label>{{ $t("label.guide_num") }}</label
+          ><input
+            type="number"
+            :placeholder="$t('label.detail.guide_num')"
+            id="guide_num"
+            required
+          />
+          <!--参加可否入力期限-->
+          <label>{{ $t("label.schedule_input_deadline") }}</label
+          ><input
+            type="datetime-local"
+            :placeholder="schedule_input_deadline"
+            id="schedule_input_deadline"
+            required
+          />
+          <!--リマインド日-->
+          <label>{{ $t("label.remind_date") }}</label
+          ><input
+            type="date"
+            :placeholder="remind_date"
+            id="remind_date"
+            required
+          />
+          <!--メモ-->
+          <label>{{ $t("label.memo") }}</label
+          ><textarea cols="30" rows="5" name="memo"> </textarea>
+        </div>
+        <br />
+        <div class="form-button-frame">
+          <button type="submit" class="button-green">
+            {{ $t("button.send") }}
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import api from "@/mixins/api";
+
+export default {
+  data() {
+    return {
+      test: "",
+    };
+  },
+  created() {},
+  methods: {
+    form_reset() {
+      const forms = document.getElementsByTagName("input");
+      for (let i = 0; i < forms.length; i += 1) {
+        forms[i].value = null;
+      }
+    },
+    async create() {
+      // ロード中にする
+      this.$emit("SendLoadComplete", false);
+
+      // アカウント作成情報を送信
+      const response = await api.post(
+        "/api/v1/createaccount",
+        {
+          email: document.getElementById("email").value,
+          name: document.getElementById("name").value,
+        },
+        this.$router.push
+      );
+
+      // API完了
+      if (response.status === "success") {
+        // 成功
+        this.form_reset();
+        this.$router.push("?status=200");
+      } else {
+        // 失敗
+        this.$router.push("?status=500").catch(() => {});
+      }
+
+      this.$emit("SendLoadComplete", true);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.create-account-page {
+  max-width: 100%;
+  position: relative;
+}
+
+.create-info {
+  background-color: var(--color-light-gray);
+  border-radius: 1em;
+  text-align: center;
+  padding: 2em;
+}
+
+.form-frame {
+  margin: 0 auto;
+  width: 100%;
+  max-width: 640px;
+  background-color: var(--color-light-gray);
+  box-sizing: border-box;
+  border-radius: 0.5em;
+}
+
+.form-tabel {
+  width: 80%;
+  display: inline-grid;
+  grid-template-columns: auto 1fr;
+  grid-row-gap: 0.5em;
+}
+
+.form-tabel label {
+  height: 100%;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+}
+
+.form-main {
+  padding: 2em;
+}
+
+.form-button-frame {
+  padding: 2em 2em 0;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+}
+</style>
