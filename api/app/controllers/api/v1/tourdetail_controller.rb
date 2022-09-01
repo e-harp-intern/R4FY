1ea9624c51ccfd8b8bbbe51fd85a9e9b2002a064
MotomaeTougo <tougo0515@ezweb.ne.jp>
2022-09-01 10:50:25 +0900

    #12 トランザクション処理追加

--- api/app/controllers/api/v1/tourdetail_controller.rb
+++ api/app/controllers/api/v1/tourdetail_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::TourdetailController < ApplicationController
   # ツアー詳細を表示
   def index
-    guideschedules = GuideSchedule.where(tour_id: params[:id])
-    tours = Tour.find_by(id: params[:id])
-    tourguides = TourGuide.where(tour_id: params[:id])
+    ActiveRecord::Base.transaction do
+      guideschedules = GuideSchedule.where(tour_id: params[:id])
+      tours = Tour.find_by(id: params[:id])
+      tourguides = TourGuide.where(tour_id: params[:id])
 
-    response = { tour: tours, guideschedule: guideschedules, tourguide: tourguides }
-    render json: json_render_v1(true, response)
+      response = { tour: tours, guideschedule: guideschedules, tourguide: tourguides }
+      render json: json_render_v1(true, response)
+    end
   end
 end
