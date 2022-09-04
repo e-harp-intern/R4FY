20a5749ea039d8dadb57cd78b4d1f1f84c3b9e1c
TomSuzuki <tomsuzuki2633@gmail.com>
2022-09-04 19:27:56 +0900

    #97 ツアー追加からトランザクション処理を削除

--- api/app/controllers/api/v1/tours_controller.rb
+++ api/app/controllers/api/v1/tours_controller.rb
@@ -12,24 +12,20 @@ class Api::V1::ToursController < ApplicationController
 
   # 　ツアーの追加
   def create
-    # ツアー予定追加に必要な情報を取得
-    ApplicationRecord.transaction do
-      tour = Tour.new(name: params[:name], start_datetime: params[:start_datetime],
-                      end_datetime: params[:end_datetime], adult_num: params[:adult_num], child_num: params[:child_num], guide_num: params[:guide_num], schedule_input_deadline: params[:schedule_input_deadline], remind_date: params[:remind_date], memo: params[:memo], sent_remind: false)
-      tour.save!
-      # 空の参加可否情報を生成
-      guides = Guide.where(is_invalid: false)
+    # 新しいツアーを作成
+    tour = Tour.new(name: params[:name], start_datetime: params[:start_datetime],
+                    end_datetime: params[:end_datetime], adult_num: params[:adult_num], child_num: params[:child_num], guide_num: params[:guide_num], schedule_input_deadline: params[:schedule_input_deadline], remind_date: params[:remind_date], memo: params[:memo], sent_remind: false)
+    tour.save!
 
-      # 追加した予定に対してガイドスケジュール,トークンを作成
-      for guide in guides do
-        GuideSchedule.create(tour_id: tour.id, guide_id: guide.id)
-        Token.new(token: generate_token, tour_id: tour.id, guide_id: guide.id).save!
-      end
+    # ガイドのリストを取得（削除済みをのぞく）
+    guides = Guide.where(is_invalid: false)
 
-    rescue ActiveRecord::RecordInvalid
-      render json: json_render_v1(false)
-      return
+    # 追加した予定に対してガイドスケジュール,トークンを作成
+    for guide in guides do
+      GuideSchedule.create(tour_id: tour.id, guide_id: guide.id)
+      Token.new(token: generate_token, tour_id: tour.id, guide_id: guide.id).save!
     end
+
     # 予定が作成されたら成功表示
     render json: json_render_v1(true)
   end
