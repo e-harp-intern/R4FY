50c048800b8a1b5c5b5930bc7cbb1a58f2bca2ac
Kento.Kanaya <zeal20410028@fsgcl.jp>
2022-09-06 15:58:24 +0900

    #96　アカウントのところにget "admin/me" => "admin#index"を移動

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
           # アカウント
           post "guides" => "guides#create"
+          get "admins/me" => "admins#index"
           post "admins" => "admins#create"
 
           # ツアー
@@ -15,9 +16,6 @@ Rails.application.routes.draw do
           get "tours" => "tours#index"
           get "tours/:id" => "tour#index"
           delete "tours/:id" => "tour#destroy"
-
-          #　管理者の名前を表示する
-          get "/admins/me" => "admins#index"
         end
     end
 
