fb9dd016c1911abbbc7f77ed1ac257c2452c5ee4
Kento.Kanaya <zeal20410028@fsgcl.jp>
2022-09-07 14:33:07 +0900

    #145　API「管理者の自分自身の情報を取得する」のpassword_digestを取り除く

--- api/app/models/application_record.rb
+++ api/app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  # 　API「管理者の自分自身の情報を取得する」のpassword_digestを取り除く
+  def as_json(opts = {})
+    opts[:except] ||= [
+      :password_digest
+    ]
+    super(opts)
+  end
 end
