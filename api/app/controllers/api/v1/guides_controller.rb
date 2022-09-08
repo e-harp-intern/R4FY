095290ccaacb9ad2d90f7ed6c8c7df9533f99ed3
Takayama <b1020117@fun.ac.jp>
2022-09-08 11:17:19 +0900

    #72

--- api/app/controllers/api/v1/guides_controller.rb
+++ api/app/controllers/api/v1/guides_controller.rb
@@ -27,13 +27,16 @@ class Api::V1::GuidesController < ApplicationController
   # ガイド情報・関連したツアー情報の表示
   def index
     token = Token.find_by(token: params[:token])
-    guide = Guide.find_by(id: token.guide_id)
-    tour = Tour.find_by(id: token.tour_id)
-
-    response = {
-      guide: JSON.parse(guide.to_json),
-      tour: JSON.parse(tour.to_json)
-    }
-    render json: json_render_v1(true, response)
+    if token.nil?
+      render json: json_render_v1(false)
+    else
+      guide = Guide.find_by(id: token.guide_id)
+      tour = Tour.find_by(id: token.tour_id)
+      response = {
+        guide: JSON.parse(guide.to_json),
+        tour: JSON.parse(tour.to_json)
+      }
+      render json: json_render_v1(true, response)
+    end
   end
 end
