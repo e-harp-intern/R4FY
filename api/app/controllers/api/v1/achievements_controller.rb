e40a432608c1909c4ae4600b7b2ec212ea8f84fc
Takayama <b1020117@fun.ac.jp>
2022-09-09 14:00:51 +0900

    #122 ガイドの実績を登録するAPIを作成 の修正

--- api/app/controllers/api/v1/achievements_controller.rb
+++ api/app/controllers/api/v1/achievements_controller.rb
@@ -5,8 +5,20 @@ class Api::V1::AchievementsController < ApplicationController
   # 実績を入力
   def create
     # createですが、updateで保存しています
-    achievements = TourGuide.find_by(id: params[:tour_id], id: params[:guide_id])
-    achievements.update(achievements_entered: achievements)
+    tour_id = Tour.find_by(id: params[:tour_id]).id
+    guide_id = Guide.find_by(id: params[:guide_id]).id
+    achievements = TourGuide.find_by(tour_id: tour_id, guide_id: guide_id)
+    attend = params[:attend]
+    memo = params[:memo]
+    # 実績入力
+    achievements.update(achievements_entered: true)
+
+    # 出席だけ入力
+    achievements.update(attend: attend) unless attend.nil?
+    # メモだけ入力
+    achievements.update(memo: memo) unless memo.nil?
+
+    # 結果を出力
     render json: json_render_v1(true, achievements)
   end
 end
