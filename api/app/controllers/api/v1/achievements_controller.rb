9ae5f59fefefe46f47b128392e62b457892ae209
Takayama <b1020117@fun.ac.jp>
2022-09-09 15:30:14 +0900

    #122 コメント改行、その他改良

--- api/app/controllers/api/v1/achievements_controller.rb
+++ api/app/controllers/api/v1/achievements_controller.rb
@@ -5,16 +5,18 @@ class Api::V1::AchievementsController < ApplicationController
   # 実績を入力
   def create
     # createですが、updateで保存しています
-    tour_id = Tour.find_by(id: params[:tour_id]).id
-    guide_id = Guide.find_by(id: params[:guide_id]).id
+    tour_id = params[:tour_id]
+    guide_id = params[:guide_id]
     achievements = TourGuide.find_by(tour_id: tour_id, guide_id: guide_id)
     attend = params[:attend]
     memo = params[:memo]
+
     # 実績入力
     achievements.update(achievements_entered: true)
 
     # 出席だけ入力
     achievements.update(attend: attend) unless attend.nil?
+
     # メモだけ入力
     achievements.update(memo: memo) unless memo.nil?
 
