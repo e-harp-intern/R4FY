b7fb0011efa7b51dc8d31a2879e4e80fac52e550
TomSuzuki <tomsuzuki2633@gmail.com>
2022-09-10 20:41:57 +0900

    #204 NotFoundページを追加

---
+++ web/src/views/NotFoundView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ $t("app_name") }}</h1>
+    <h2>{{ $t("pages.notfound.title") }}</h2>
+    <p>
+      <button
+        onclick="location.href='/'"
+        class="link button-large button-green"
+      >
+        {{ $t("pages.notfound.link") }}
+      </button>
+    </p>
+    <p class="info">{{ $t("pages.notfound.info") }}</p>
+  </div>
+</template>
+
+<style scoped>
+.link {
+  margin: 3em;
+}
+
+.info {
+  padding: 2em;
+  box-sizing: border-box;
+}
+</style>
