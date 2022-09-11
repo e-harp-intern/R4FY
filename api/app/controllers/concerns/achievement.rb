b640f41d16c744186254419469cc50632db83b57
TomSuzuki <tomsuzuki2633@gmail.com>
2022-09-11 12:41:19 +0900

    #175 実績一覧でレスポンスの名前を変更

--- api/app/controllers/concerns/achievement.rb
+++ api/app/controllers/concerns/achievement.rb
@@ -13,22 +13,22 @@ module Achievement
     achievement["guide_id"] = guide_id
 
     # 前回出席ツアーを検索
-    achievement["lasttime"] = TourGuide
-                              .where(guide_id: guide_id, achievements_entered: true) # 指定ガイドの入力済み実績のみ取得
-                              .joins(:tour) # ツアーを結合
-                              .select("tours.*") # ツアー情報のみを取得
-                              .where.not("tour_state_code = ?", TOUR_STATE_CODE_CANCEL) # 中止ツアーを除く
-                              .order(start_datetime: "DESC") # 開始日順に並び替え（可能なら最大値を使用）
-                              .first # 先頭の開始日のみ取得
+    achievement["last_tour"] = TourGuide
+                               .where(guide_id: guide_id, achievements_entered: true) # 指定ガイドの入力済み実績のみ取得
+                               .joins(:tour) # ツアーを結合
+                               .select("tours.*") # ツアー情報のみを取得
+                               .where.not("tour_state_code = ?", TOUR_STATE_CODE_CANCEL) # 中止ツアーを除く
+                               .order(start_datetime: "DESC") # 開始日順に並び替え（可能なら最大値を使用）
+                               .first # 先頭の開始日のみ取得
 
     # 最新 tour_num 回 の出席したツアーのリストを取得
-    achievement["participation_days"] = TourGuide
-                                        .where(guide_id: guide_id, achievements_entered: true)
-                                        .joins(:tour)
-                                        .select("tours.*")
-                                        .where.not("tour_state_code = ?", TOUR_STATE_CODE_CANCEL)
-                                        .order(start_datetime: "DESC")
-                                        .limit(tour_num)
+    achievement["participation_tours"] = TourGuide
+                                         .where(guide_id: guide_id, achievements_entered: true)
+                                         .joins(:tour)
+                                         .select("tours.*")
+                                         .where.not("tour_state_code = ?", TOUR_STATE_CODE_CANCEL)
+                                         .order(start_datetime: "DESC")
+                                         .limit(tour_num)
 
     # 過去（今日から指定月前） month 月 に出席したツアーの回数を取得
     achievement["participation_num"] = TourGuide
@@ -41,12 +41,12 @@ module Achievement
                                        .count(:id)
 
     # 参加予定のツアーのリスト（今日以降）
-    achievement["assign"] = TourGuide
-                            .where(guide_id: guide_id)
-                            .joins(:tour)
-                            .select("tours.*")
-                            .where("start_datetime > ?", today)
-                            .where("tour_state_code = ?", TOUR_STATE_CODE_ASSIGNED) # 担当者割り当て済みのみから取得
+    achievement["assign_tours"] = TourGuide
+                                  .where(guide_id: guide_id)
+                                  .joins(:tour)
+                                  .select("tours.*")
+                                  .where("start_datetime > ?", today)
+                                  .where("tour_state_code = ?", TOUR_STATE_CODE_ASSIGNED) # 担当者割り当て済みのみから取得
 
     # 返すデータ
     achievement
