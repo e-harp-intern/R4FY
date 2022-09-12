06a8b152acc28f48714e267adfbd57056ef06adf
Takayama <b1020117@fun.ac.jp>
2022-09-12 13:11:35 +0900

    #218 サーバーエラーを吐かないよう変更

--- api/app/controllers/api/v1/sessions_controller.rb
+++ api/app/controllers/api/v1/sessions_controller.rb
@@ -6,8 +6,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = Admin.find_by(email: params[:session][:email].downcase)
     session[:user_id] = nil
-    is_invalid = Admin.find_by(email: user.email).is_invalid
-    if user&.authenticate(params[:session][:password]) && is_invalid == false
+    if user&.authenticate(params[:session][:password]) && Admin.find_by(email: user.email).is_invalid == false
       # ログイン成功
       session[:user_id] = user.id
       render json: json_render_v1(true)
