e797371e3bb86d45d30b2f9fc01af952aae54a02
Takayama <b1020117@fun.ac.jp>
2022-09-13 15:53:49 +0900

    #138 管理者がパスワードを変更するAPI

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
           patch "admins/:id" => "admins#update"
           delete "admins/:id" => "admins#delete"
           get "admins/:id" => "admins#index"
+          post "password/:token" => "password#create"
 
           # ガイド
           post "guides" => "guides#create"
