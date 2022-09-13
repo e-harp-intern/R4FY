a1100f0273fce2cc13ce413532a8c342d656af0f
Takayama <b1020117@fun.ac.jp>
2022-09-13 15:14:35 +0900

    #74 インデントや変数名の変更

--- api/app/controllers/api/v1/tour_controller.rb
+++ api/app/controllers/api/v1/tour_controller.rb
@@ -21,28 +21,34 @@ class Api::V1::TourController < ApplicationController
   # ツアーを中止にする
   def destroy
     tour_delete = Tour.find_by(id: params[:id])
-    achievements = TourGuide.find_by(tour_id: params[:id]).achievements_entered
+
     # 担当者決定後の場合
-    guides_id = if tour_delete.tour_state_code == TOUR_STATE_CODE_ASSIGNED || achievements == true
-                  TourGuide.where(id: params[:id])
+    if tour_delete.tour_state_code == TOUR_STATE_CODE_ASSIGNED || TourGuide.find_by(tour_id: params[:id]).achievements_entered == true
+      tour_guides = TourGuide.where(id: params[:id])
 
-                # 担当者のみにメールを送信
+      # 担当者のみにメールを送信
+      tour_guides.each do |tour_guides|
+        guides = Guide.find_by(id: tour_guides.guide_id)
+        TourCancelNotifyMailer.cancel_email(guides, tour_delete).deliver_now
+      end
 
-                # ツアー状態をキャンセルに変更
-                else
-                  # 担当者決定前の場合
-                  GuideSchedule.where(tour_id: params[:id])
+      # ツアー状態をキャンセルに変更
+      tour_delete.update(tour_state_code: TOUR_STATE_CODE_CANCEL)
+      render json: json_render_v1(true)
+    else
+      # 担当者決定前の場合
+      guide_schedules = GuideSchedule.where(tour_id: params[:id])
 
-                  # 全員にメールを送信
+      # 全員にメールを送信
+      guide_schedules.each do |guide_schedules|
+        guides = Guide.find_by(id: guide_schedules.guide_id)
+        TourCancelNotifyMailer.cancel_email(guides, tour_delete).deliver_now
+      end
 
-                  # ツアー状態をキャンセルに変更
-                end
-    guides_id.each do |guides_id|
-      guides = Guide.find_by(id: guides_id.guide_id)
-      TourCancelNotifyMailer.cancel_email(guides, tour_delete).deliver_now
+      # ツアー状態をキャンセルに変更
+      tour_delete.update(tour_state_code: TOUR_STATE_CODE_CANCEL)
+      render json: json_render_v1(true, guide_schedules)
     end
-    tour_delete.update(tour_state_code: TOUR_STATE_CODE_CANCEL)
-    render json: json_render_v1(true)
     nil
   end
 end
