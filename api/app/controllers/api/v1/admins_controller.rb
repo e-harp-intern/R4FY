16e21826b1b2827987fbfbdf8660382392069eb8
Takayama <b1020117@fun.ac.jp>
2022-09-13 10:28:56 +0900

    #266 パスワード変更機能など

--- api/app/controllers/api/v1/admins_controller.rb
+++ api/app/controllers/api/v1/admins_controller.rb
@@ -58,18 +58,36 @@ class Api::V1::AdminsController < ApplicationController
     render json: json_render_v1(true)
   end
 
-  # 　管理者情報の変更をする
+  # 管理者情報の変更をする
   def update
     admin = Admin.find_by(id: params[:id])
-    name = params[:name]
-    email = params[:email]
 
-    # 　名前だけを変更する条件
-    admin.update(name: name) unless name.nil?
+    # 変更を行う管理者が変更対象の管理者と一致するか
+    if admin == @current_user
+      name = params[:name]
+      email = params[:email]
+      password = params[:password]
 
-    # 　メールだけを変更する条件
-    admin.update(email: email) unless email.nil?
+      # 名前だけを変更する条件
+      admin.update(name: name) unless name.nil?
 
-    render json: json_render_v1(true)
+      # メールだけを変更する条件
+      admin.update(email: email) unless email.nil?
+
+      # パスワードだけを変更する条件
+      admin.update(password: password) unless password.nil?
+
+      render json: json_render_v1(true)
+      nil
+
+    # 他の管理者のIDを指定した場合
+    elsif @current_user.id != admin.id
+      render json: json_render_v1(false, status: 403)
+      nil
+
+    # それ以外
+    else
+      render json: json_render_v1(false)
+    end
   end
 end
