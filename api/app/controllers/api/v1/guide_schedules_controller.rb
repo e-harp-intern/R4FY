a02b970e7a476c767b2ccac092d48568e52ddaeb
Takayama <b1020117@fun.ac.jp>
2022-09-14 14:35:48 +0900

    #227 実績入力時のツアー状態コードの変更

--- api/app/controllers/api/v1/guide_schedules_controller.rb
+++ api/app/controllers/api/v1/guide_schedules_controller.rb
@@ -22,10 +22,15 @@ class Api::V1::GuideSchedulesController < ApplicationController
       return
     end
 
+    # 参加したガイド全員の実績を入力していない場合、ツアー状態コードを実施済みへ変更する
+    tour_guides = TourGuide.where(tour_id: tour.id, achievements_entered: false)
+    count = tour_guides.count
+    tour.update(tour_state_code: TOUR_STATE_CODE_COMPLETE) if count != 0
+
     # ガイドアカウントが有効な場合
     guide_schedules = GuideSchedule.find_by(guide_id: token.guide_id, tour_id: token.tour_id)
     guide_schedules.update(answered: true, possible: params[:possible])
-    render json: json_render_v1(true, guide)
+    render json: json_render_v1(true)
   end
 
   # ガイド情報・関連したツアー情報・入力済の参加可否情報の表示
