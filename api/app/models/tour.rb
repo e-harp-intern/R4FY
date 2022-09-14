2a09055714c5e3afd041183ff82c5b2b7ab31ac9
TomSuzuki <tomsuzuki2633@gmail.com>
2022-09-14 14:26:59 +0900

    #317 マイナスの人数を登録できないようにバリデーションを追加

--- api/app/models/tour.rb
+++ api/app/models/tour.rb
@@ -4,4 +4,7 @@ class Tour < ApplicationRecord
   has_many :tour_guides
   has_many :tokens
   validates :end_datetime, comparison: { greater_than: :start_datetime }
+  validates :adult_num, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :child_num, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :guide_num, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
