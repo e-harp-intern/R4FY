cec551aa8ef9a1a86fe1d1fa6386d5a4a4c364d7
Takayama <b1020117@fun.ac.jp>
2022-09-15 15:50:16 +0900

    #195 ツアー作成後アカウント用ツアースケジュール入力機能

---
+++ api/app/controllers/api/v1/tour_schedules_controller.rb
@@ -0,0 +1,28 @@
+require "securerandom"
+
+class Api::V1::TourSchedulesController < ApplicationController
+  include Schedule
+  include Common
+  before_action :require_login
+
+  def create
+    # パラメーター
+    guide_schedule = GuideSchedule.find_by(tour_id: params[:tour_id], guide_id: params[:guide_id])
+
+    # ガイドスケジュールが作成済の場合
+    unless guide_schedule.nil?
+      render json: json_render_v1(false)
+      return
+    end
+
+    # ガイドスケジュールが作成されていなかった場合
+    GuideSchedule.create(tour_id: params[:tour_id], guide_id: params[:guide_id])
+    token = Token.new(token: generate_token, tour_id: params[:tour_id], guide_id: params[:guide_id])
+    token.save!
+
+    # メール送信
+    schedule(Guide.find_by(id: params[:guide_id]), token)
+
+    render json: json_render_v1(true)
+  end
+end
