d8a14695260d1bfa79d1b0692907151ac7ef35d7
TomSuzuki <tomsuzuki2633@gmail.com>
2022-09-19 16:20:48 +0900

    #231 担当ガイド決定時に各種メールを送信するよう変更

--- api/app/controllers/api/v1/tour_guides_controller.rb
+++ api/app/controllers/api/v1/tour_guides_controller.rb
@@ -15,11 +15,11 @@ class Api::V1::TourGuidesController < ApplicationController
 
     ApplicationRecord.transaction do
       # 担当から外れたガイドを取得
-      guides = TourGuide.where(tour_id: tour_id).where(guide_id: guides)
-      guides.each do |g|
+      del_guides = TourGuide.where(tour_id: tour_id).where.not(guide_id: guides)
+      del_guides.each do |g|
         remove_assign.push(g)
       end
-      guides.destroy_all
+      del_guides.destroy_all
 
       # 担当ガイドを追加（TODO: 削除済みのガイドを指定した場合はエラー）
       guides.each do |g|
@@ -32,26 +32,25 @@ class Api::V1::TourGuidesController < ApplicationController
 
       # ツアーの状態を担当者決定済みに更新
       is_first_assign = (tour.tour_state_code < TOUR_STATE_CODE_ASSIGNED)
-      tour.update(tour_state_code: TOUR_STATE_CODE_ASSIGNED)
+      tour.update(tour_state_code: TOUR_STATE_CODE_ASSIGNED) if is_first_assign
 
       # 成功時にsend_mailがtrueなら担当者にメールを送信
       if send_mail == true
         # 新しく担当者になった人へメール送信
         new_assign.each do |g|
-          guide = Guide.find_by(id: g)
-          AssignNotifyMailer.creation_email(guide, tour).deliver_now
+          AssignNotifyMailer.creation_email(Guide.find(g.guide_id), tour).deliver_now
         end
 
         # 担当から外れた場合に送信
         remove_assign.each do |g|
-          # TODO: 割り当てから外れたメールを送信
+          AssignCancelNotifyMailer.cancel_email(Guide.find(g.guide_id), tour).deliver_now
         end
 
         # 担当にならなかった場合に送信
         if is_first_assign
-          guides = TourGuide.where(tour_id: tour_id).where.not(guide_id: guides)
-          guides.each do |g|
-            # TODO: ならなかったメールを送信
+          no_assign_guides = GuideSchedule.where(tour_id: tour_id).where.not(guide_id: guides)
+          no_assign_guides.each do |g|
+            NotAssignMailer.creation_email(Guide.find(g.guide_id), tour).deliver_now
           end
         end
       end
