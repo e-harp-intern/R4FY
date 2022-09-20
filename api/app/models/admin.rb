5eb65a1a09205ea9bb5dc76179b82d117656003a
TomSuzuki <tomsuzuki2633@gmail.com>
2022-09-20 09:12:40 +0900

    #382 管理者管理機能を修正

--- api/app/models/admin.rb
+++ api/app/models/admin.rb
@@ -1,5 +1,5 @@
 class Admin < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 300 }
+  validates :email, presence: true, length: { maximum: 300 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   has_secure_password
 end
