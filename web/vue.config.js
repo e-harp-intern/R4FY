0ace1d0bb924241c446fd6334f4b0772a6add5bb
TomSuzuki <tomsuzuki2633@gmail.com>
2022-09-22 10:03:49 +0900

    #454 設定の場所を移動

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-  publicPath: "/guide/",
+  publicPath: process.env.VUE_APP_API_ORIGIN,
   transpileDependencies: true,
   configureWebpack: {
     watchOptions: {
