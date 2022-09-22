96e5b6f7cd87899e82d6c74bfafb5e85701b11d8
TomSuzuki <tomsuzuki2633@gmail.com>
2022-09-22 10:07:29 +0900

    #454 ビルドスクリプトを一部変更

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # 検証環境用ビルドスクリプト（WSL上で実行：使えない場合は手動 or windows用スクリプト作成）
 
 # Vue.js のビルド
-docker-compose run web npm run build
+docker compose run web npm run build
 
 # Rails の public 以下を全消去
 ls ./api/public | grep -v -E "robots.txt" | xargs rm -rf
