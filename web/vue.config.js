750dda4e961998e0c0388fe767f9f71aa3d1a18e
TomSuzuki <tomsuzuki2633@gmail.com>
2022-09-22 09:04:06 +0900

    #454 設定の変更

--- web/vue.config.js
+++ web/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   publicPath: "./",
+  baseUrl: "/guide",
   transpileDependencies: true,
   configureWebpack: {
     watchOptions: {
