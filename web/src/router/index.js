0616f6a2d573f676017aad1f4ede6dd492f49865
TomSuzuki <tomsuzuki2633@gmail.com>
2022-09-23 17:29:20 +0900

    #488 ページ遷移時にスクロール位置が一番上になるよう修正

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -111,10 +111,17 @@ const routes = [
   },
 ];
 
+// スクロール位置の制御
+const scrollBehavior = (to, from, savedPosition) => {
+  if (savedPosition) return savedPosition;
+  return { x: 0, y: 0 };
+};
+
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior,
 });
 
 export default router;
