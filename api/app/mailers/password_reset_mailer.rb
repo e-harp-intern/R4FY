91155a1c99f9a8454573ed2c755ced6475eecd4d
TodaKanta <0528kanta@gmail.com>
2022-09-28 17:13:58 +0900

    #512 【パスワードリセット】パスワードを再設定してください

--- api/app/mailers/password_reset_mailer.rb
+++ api/app/mailers/password_reset_mailer.rb
@@ -3,7 +3,7 @@ class PasswordResetMailer < ApplicationMailer
     @admin = admin
     @url = url
     mail(
-      subject: "【パスワードリセット通知】パスワード再設定依頼",
+      subject: "【パスワードリセット】パスワードを再設定してください",
       to: admin.email
     )
   end
